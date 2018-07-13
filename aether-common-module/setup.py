e8eb13a686306ca50176b8e21b73c4a647029a2d
obdulia <obdulia.losantos@gmail.com>
2018-07-13 15:16:17 +0200

    chore: upgrade to Django@2.1

--- aether-common-module/setup.py
+++ aether-common-module/setup.py
@@ -42,7 +42,7 @@ setup(
 
     python_requires='>=3.6',
     install_requires=[
-        'django<2',
+        'django',
         'django-cors-headers',
         'django-debug-toolbar',
         'django-prometheus',
